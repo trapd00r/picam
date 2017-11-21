c67f4ecb40610626b5d106be537052b2d1a39909
japh <m@japh.se>
2017-11-21 08:09:56 +0100

    add pisnap shell script and modify deploy script to reflect changes

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-picam
-while true; do perl twitter.pl "$(date) at \#Orrholmen."; sleep 3600; done
+pisnap
+while true; do perl twitter.pl --tweet; sleep 3600; done
 while true; do convert -resize 25% $HOME/camera/latest.jpg $HOME/camera/latest.png; sleep 3550; done &
---
+++ pisnap.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# pisnap
+# take photo, scp it to server and delete it from the pi to save space.
+
+# set up:
+#   credentials for auto-scp'ing
+#   photo save dir
+#   sleep time
+
+           USER='scp1'
+           HOST='laleh'
+ SAVE_DIR_LOCAL="${HOME}/camera"
+SAVE_DIR_REMOTE="_picam"
+          SLEEP=1730
+
+while true; do
+  FILE=$(date +"%Y-%m-%d_%H%M%S").jpg
+
+  printf -- "----------\n"
+  raspistill -vf -hf -n -o $FILE && printf "$FILE written\n"
+  scp $FILE ${USER}@${HOST}:${SAVE_DIR_REMOTE} && cp -v $FILE $SAVE_DIR_LOCAL/latest.jpg && rm -v $FILE
+  printf -- "----------\n"
+  sleep $SLEEP
+done
